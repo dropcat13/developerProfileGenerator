22986f947b9c29f9c35a4026f6728701f7a89b8a
Cat Kretzschmar <cat.ktz@gmail.com>
2020-02-08 08:54:42 -0500

    produced html

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 // const util = require("util");
 const axios = require("axios");
-// const generateHTML = require("generateHTML.js");
-const path = require("path");
+const generateHTML = require("./generateHTML");
+// const path = require("path");
 // const open = require("open");
-     
-// promptUser().then(res => console.log(res))
-promptUser().then(res => getInfo(res.username))
+// const writeFileAsync = util.promisify(fs.writeFile);
+  
+console.log(generateHTML);
+// promptUser().then(console.log(res))
+promptUser().then(res => getInfo(res))
 
 function promptUser() {
   return inquirer.prompt([
@@ -22,34 +24,33 @@ function promptUser() {
       message: "What is your favorite color?",
       choices: ['green', 'blue', 'pink', 'red'],
     }
-  ]);
-}
+  ])
+};
 
 function getInfo(res) {
   const queryUrl = `https://api.github.com/users/${res.username}`;
   const queryStars = `https://api.github.com/users/${res.username}/repos`;
-  // const colorChoice = answer.color
 
-  axios.get(queryUrl).then(function (userinfo) {
-    console.log(userinfo.data);
-    userinfo.data.color = res.color;
-    axios.get(queryStars).then(function (repos){
+    axios.get(queryUrl).then(function (userinfo) {
+      console.log(userinfo.data);
+      userinfo.data.color = res.color;
+      axios.get(queryStars).then(function (repos){
 
-      for (const repo of repos.data) {
-        totalStars += repo.stargazers_count;
-      }
+        let totalStars = 0;
+        for (const repo of repos.data) {
+          totalStars += repo.stargazers_count;
+        }
+        console.log(totalStars);
+        userinfo.data.stars = totalStars;
+        const generateContent = generateHTML(userinfo.data);
+          console.log(generateContent);
+          fs.writeFile("resume.html", generateContent, function(err) {
 
-      const generateContent = generateHTML(userinfo.data);
-        console.log(generateContent);
-           
-    }).catch(function (err){
-      console.log("user not found");
-      process.exit(1);
-    });
-          
-  })
-}
-
-app.get("/", function(req, res) {
-  res.sendfile(_dirname + 'generateHTML.js');
-});
+          });    
+      }).catch(function (err){
+        console.log("user not found");
+        process.exit(1);
+      });
+            
+    })
+};
